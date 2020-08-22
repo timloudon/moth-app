4b6fb620050828f6471b635f24f07bea7d4a74e6
Tim Loudon <timloudon@gmail.com>
2020-08-22 13:54:22 +0100

    Installed Material UI

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from '../Home/Home';
 import Exercise from '../Exercise/Exercise';
 import About from '../About/About';
+import Album from '../Test/Album';
 import Splash from './Splash';
 import { Link, Route, Switch } from 'react-router-dom';
 
@@ -12,10 +13,12 @@ function App() {
       <Link to="/">Home</Link>
       <Link to="/../Exercise">Exercise</Link>
       <Link to="/../About">About</Link>
+      <Link to="/../Test">Album</Link>
       <Switch>
-        <Route component={ Home } exact path="/" />
-        <Route component={ Exercise } path="/../Exercise" />
-        <Route component={ About } path="/../About" />
+        <Route component={Home} exact path="/" />
+        <Route component={Exercise} path="/../Exercise" />
+        <Route component={About} path="/../About" />
+        <Route component={Album} path="/../Test" />
       </Switch>
     </div>
   );
