9cbaabfa7bbd29af9f8662f26622efc57084df56
Tim Loudon <timloudon@gmail.com>
2021-01-19 20:56:43 +0000

    refactoring of Exercise funcitons into helperFunctions

--- src/components/Exercises/SamplesProvider.js
+++ src/components/Exercises/SamplesProvider.js
@@ -7,12 +7,6 @@ import SpinnerLogo from '../common/SpinnerLogo';
 function SamplesProvider({ routeProps, instrumentType, ctx }) {
     const [isLoading, samples] = useSamples(instruments, instrumentType, ctx);
 
-    // if (isLoading) { return <div>Loading...</div> }
-    // if (!isLoading) { return (<Exercise routeProps={routeProps} ctx={ctx} instrumentType={instrumentType} samples={samples} />) }
-    // let display;
-    // if (isLoading) { display = <div>Loading...</div> }
-    // if (!isLoading) { display = (<Exercise isLoading={isLoading} routeProps={routeProps} ctx={ctx} instrumentType={instrumentType} samples={samples} />) }
-    // return (display);
     return (
         <>
             {isLoading
---
+++ src/resources/helperFunctions.js
@@ -0,0 +1,29 @@
+import { scalePatterns } from "./musicResources";
+
+export function getScaleNotes(scalePattern, allNotes) {
+    let scaleNotes = [];
+    let firstNote = allNotes[0];
+    let currentMidiNumber;
+    let nextMidiNumber = firstNote.midiNumber;
+    let lastAvailableMidiNumber = allNotes[allNotes.length - 1].midiNumber;
+
+    scaleNotes.push(firstNote);
+
+    for (let i = 0; nextMidiNumber < lastAvailableMidiNumber; i++) {
+        let patternIndex = i % scalePattern.length;
+        let increment = scalePattern[patternIndex];
+        currentMidiNumber = scaleNotes[i].midiNumber;
+        nextMidiNumber = currentMidiNumber + increment;
+        let nextNote = allNotes.find((note) => note.midiNumber === nextMidiNumber);
+        scaleNotes.push(nextNote);
+    }
+    return scaleNotes;
+}
+
+export const findScalePattern = (scaleType) => {
+    const scaleObject = scalePatterns.find(
+        (pattern) => pattern.type === scaleType
+    );
+    const scalePattern = scaleObject.pattern;
+    return scalePattern;
+};
